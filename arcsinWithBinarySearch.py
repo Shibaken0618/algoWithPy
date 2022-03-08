4d830270eea214225b58d4af67ccb924da55687b
Kent <60073215+Shibaken0618@users.noreply.github.com>
2022-03-07 19:44:39 -0500

    done

--- arcsinWithBinarySearch.py
+++ arcsinWithBinarySearch.py
@@ -2,10 +2,10 @@ from sortedBinarySearch import binarySearch
 import math
 
 def inverseSin(number):
- domain = [x * math.pi/10000 - math.pi/2 for x in list(range(0,10000))]
- the_range = [math.sin(x) for x in domain]
- result = domain[binarySearch(the_range,number)]
- return(result)
+    domain = [x * math.pi/10000 - math.pi/2 for x in list(range(0,10000))]
+    the_range = [math.sin(x) for x in domain]
+    result = domain[binarySearch(the_range,number)]
+    return(result)
 
 arcSin = inverseSin(0.9)
 print(arcSin)

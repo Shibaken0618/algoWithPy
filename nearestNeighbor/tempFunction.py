7838dba1378b23ac3e53c898a9cb065987f0dbf6
Kent <60073215+Shibaken0618@users.noreply.github.com>
2022-03-11 17:35:53 -0500

    done

--- nearestNeighbor/tempFunction.py
+++ nearestNeighbor/tempFunction.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+
+from pandas import MultiIndex
 from TSP import howFar,genlines
 
 temperature = lambda t:1/(t+1)
@@ -80,3 +82,71 @@ def perturb_sa2(cities,itinerary,time):
         itineraryToReturn = itinerary2.copy()
     
     return itineraryToReturn.copyy()
+
+
+def perturb_sa3(cities,itinerary,time,maxitin):
+    neighborids1 = math.floor(np.random.rand()*(len(itinerary)))
+    neighborids2 = math.floor(np.random.rand()*(len(itinerary)))
+    global mindistance
+    global minitinerary
+    global minidx
+
+    itinerary2 = itinerary.copy()
+    randomdraw2 = np.random.rand()
+    small = min(neighborids1,neighborids2)
+    big = max(neighborids1,neighborids2)
+    if randomdraw2 >= 0.55:
+        itinerary2[small:big] = itinerary2[small:big][::-1]
+    elif randomdraw2 < 0.45:
+        tempitin = itinerary[small:big]
+        del(itinerary2[small:big])
+        neighborids3 = math.floor(np.random.rand()*(len(itinerary)))
+        for j in range(0,len(tempitin)):
+            itinerary2.insert(neighborids3 + j,tempitin[j])
+    else:
+        itinerary2[neighborids1] = itinerary[neighborids2]
+        itinerary2[neighborids2] = itinerary[neighborids1]
+
+    distance1 = howFar(genlines(cities,itinerary)) 
+    distance2 = howFar(genlines(cities,itinerary2)) 
+
+    itineraryToReturn = itinerary.copy()
+    randomdraw = np.random.rand()
+    temperature = 1/(time/(maxitin/10) + 1)
+
+    scale = 3.5
+    if((distance2 > distance1 and (randomdraw) < (math.exp(scale*(distance1-distance2))*
+    temperature)) or (distance1 > distance2)):
+        itineraryToReturn = itinerary2.copy()
+    
+    reset = True
+    resetthresh = 0.04
+    if(reset and (time-minidx) > (maxitin*resetthresh)):
+        itineraryToReturn = minitinerary
+        minidx = time
+    
+    if(howFar(genlines(cities,itineraryToReturn))<mindistance):
+        mindistance = howFar(genlines(cities,itinerary2))
+        minitinerary = itineraryToReturn
+        minidx = time
+
+    if(abs(time - maxitin) <= 1):
+        itineraryToReturn = minitinerary.copy()
+
+    return itineraryToReturn.copyy()
+
+
+def siman(itinerary,cities):
+    newitinerary = itinerary.copy()
+    global mindistance
+    global minitinerary
+    global minidx
+    mindistance = howFar(genlines(cities,itinerary))
+    minitinerary = itinerary
+    minidx = 0
+    
+    maxitin = len(itinerary) * 50000
+    for t in range(0,maxitin):
+        newitinerary = perturb_sa3(cities,newitinerary,t,maxitin)
+    
+    return(newitinerary.copy())

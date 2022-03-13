4a96f66c4e41c19e898f9f53a857d1ba9e20fb48
Kent <60073215+Shibaken0618@users.noreply.github.com>
2022-03-12 21:32:05 -0500

    done

--- greedyAlgo.py
+++ greedyAlgo.py
@@ -8,5 +8,19 @@ stations["kthree"] = set(["or", "nv", "ca"])
 stations["kfour"] = set(["nv", "ut"])
 stations["kfive"] = set(["ca", "az"])
 
-print(stations)
+final_stations = set()
+
+while states_needed:
+    best_station = None
+    states_covered = set()
+    for station, states_for_station in stations.items():
+        covered = states_needed and states_for_station
+        if len(covered) > len(states_covered):
+            best_station = station
+            states_covered = covered
+
+states_needed -= states_covered
+final_stations.add(best_station)
+
+print(final_stations)
 
---
+++ playingWithSets.py
@@ -0,0 +1,7 @@
+fruits = set(["avocado",'tomato','banana'])
+vegetables = set(["beets","carrots","tomato"])
+print(fruits | vegetables)
+print(fruits & vegetables)
+print(fruits - vegetables)
+print(vegetables - fruits)
+
